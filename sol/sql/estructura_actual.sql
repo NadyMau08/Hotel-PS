a89fb06f604462ce82e1c405dcd10b3bca45b5f6
Nadia Michelle <nnadiamichelle@gmail.com>
2025-08-01 11:00:41 -0600

    Delete sol directory

--- sol/sql/estructura_actual.sql
+++
@@ -1,5 +0,0 @@
--- Script para mostrar la estructura actual de la base de datos
-SHOW TABLES FROM hotel_management;
-
--- Para ver la estructura de cada tabla (ejecutar por separado para cada tabla)
--- SHOW COLUMNS FROM users;
